c3fe7df7da9afab2cb0e825d137f6aadcdabc373
blasterwhite <Blaster>
2022-01-15 14:39:31 +0100

    Changed to shaker sort

--- TrisEtape5/covid.c
+++ TrisEtape5/covid.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define MAX 17776
 
@@ -16,54 +17,72 @@ typedef struct{
 typedef donnees_covid t_covid[MAX];
 
 
-void fusion_c(t_covid tableau, int debutDeuxiemeMoitier, int finPremiereMoitier, int finDuTableau, long *comparaison, long *permutation) {
-    donnees_covid table1[finPremiereMoitier - debutDeuxiemeMoitier+1];
-    int debutPremiereMoitier = finPremiereMoitier+1;
-    int compt1 = debutDeuxiemeMoitier;
-    int compt2 = debutPremiereMoitier;
-    int i;
-    bool pasTrie = true;
-    for(i = debutDeuxiemeMoitier; i <= finPremiereMoitier; i++){
-        table1[i - debutDeuxiemeMoitier] = tableau[i];
-    }
-    i = debutDeuxiemeMoitier;
-    while ((i <= finDuTableau) && pasTrie){
-        *comparaison = *comparaison + 1; // Ajout d'une comparaison
-        if (compt1 == debutPremiereMoitier) { // met que le tableau est trie 
-            pasTrie = false;
-        } else if (compt2 == (finDuTableau+1)) {
-            tableau[i] = table1[compt1 - debutDeuxiemeMoitier]; // Si le compteur2 est arrivé a la fin du tableau trié 2 on met compteur1 valeur du tableau 1 a la suite du tableau final
-            compt1++;
-            *permutation = *permutation + 1; // Ajout d'une permutation 
-        }else if (table1[compt1 - debutDeuxiemeMoitier].dep < tableau[compt2].dep){// Si la premiere valeur du tableau trie 1 et plus petite que la première valeur du tableau trie 2 alors on la met a l'indice i du tableau final
-            tableau[i] = table1[compt1 - debutDeuxiemeMoitier];
-            compt1++;
-            *permutation = *permutation + 1; // Ajout d'une permutation 
-        } else { // Si aucune des autre condition a marche c'est que la premiere valeur du tableau trie 2 et plus petite que la première valeur du tableau trie 1 donc on la met a l'indice i du tableau final
-            tableau[i] = tableau[compt2];
-            compt2++;
+void permute(donnees_covid t1, donnees_covid t2, long *permutation) {
+    *permutation = *permutation +1;
+    donnees_covid temp;
+    strcpy(temp.date, t1.date);
+    temp.classe = t1.classe;
+    temp.dep = t1.dep;
+    temp.pop = t1.pop;
+    temp.pos = t1.pos;
+    strcpy(t1.date,t2.date);
+    t1.classe = t2.classe;
+    t1.dep = t2.dep;
+    t1.pop = t2.pop;
+    t1.pos = t2.pos;
+    strcpy(t2.date,temp.date);
+    t2.classe = temp.classe;
+    t2.dep = temp.dep;
+    t2.pop = temp.pop;
+    t2.pos = temp.pos;
+}
+
+void shaker_croiss (t_covid t,long *permutation, long *comparaison) {
+    int i = 0; // indice du nombre d"éléments déjà trié
+    int trie = 0; // Dit si le trie est fini
+    while (trie == 0) {
+        trie = 1;
+        for (int a = i+1; a < MAX-i; a++) { // On fait monter le plus grands nombre du tableau
+            *comparaison = *comparaison+1;
+            if (t[a].pop < t[a-1].pop) {
+                //printf("PERMUTE %d et %d\n", a, a-1);
+                trie = 0; // Dit que le trie n'est pas terminé si il y a du changement
+                permute(t[a],t[a-1],&*permutation);
+            }
         }
-        i++;
+        if (trie == 0) {
+            for (int b = MAX-i-2; b >= i; b--) { // On fait descendre le plus petit nombre du tableau
+                *comparaison = *comparaison +1;
+                if (t[b].pop > t[b-1].pop) {
+                    //printf("PERMUTE %d et %d\n", b, b+1);
+                    trie = 0; // Dit que le trie n'est pas terminé si il y b du changement
+                    permute(t[b],t[b+1],&*permutation);
+                }
+            }
+        }
+        i=i+1;
     }
 }
 
-void fusion_croiss_recur(t_covid tableau, int debutTableau, int finTableau, long *comparaison, long *permutation){
-    int milieu;
-    if (debutTableau < finTableau){
-        milieu = (finTableau + debutTableau) / 2;
-        fusion_croiss_recur(tableau, debutTableau, milieu, &*comparaison, &*permutation);
-        fusion_croiss_recur(tableau, milieu + 1, finTableau, &*comparaison, &*permutation);
-        fusion_c(tableau, debutTableau, milieu, finTableau, &*comparaison, &*permutation);
-        //affiche(tableau); // Pour comprendre les étapes
-    }
+void debutMesuresPerf(clock_t t1, long *comparaison, long *permutation) {
+    t1 = clock();
+    *comparaison = 0;
+    *permutation = 0;
 }
 
+void finMesuresPerf(clock_t t1, long comparaison, long permutation) {
+    clock_t t2;
+    float temps;
 
-void fusion_croiss(t_covid tableau, long *comparaison, long *permutation){
-    if (MAX > 0){
-        fusion_croiss_recur(tableau, 0, MAX - 1, &*comparaison, &*permutation);
-    }
+    t2 = clock();
+    temps = (float)(t2-t1)/CLOCKS_PER_SEC;
+    printf("        TRI PAR MACHIN        \n\n");
+    printf("Temps CPU du tri : %3fs\n\n", temps);
+    printf("Nombre de comparaisons : %ld\n\n", comparaison);
+    printf("Nombre de Permutation : %ld\n\n", permutation);
+
 }
+
 
 
 
@@ -87,7 +106,7 @@ void triDepartement(t_covid t)
 {
     long comp;
     long perm;
-    fusion_croiss(t,&comp,&perm);
+    shaker_croiss(t,&comp,&perm);
 }
 
 
@@ -95,7 +114,7 @@ void afficher(t_covid t)
 {
     for(int i=0; i<MAX; i++)
     {
-        printf("%d %s %d %d %d\n",t[i].dep, t[i].date, t[i].pos, t[i].classe, t[i].pop);
+        printf("Departement : %3d | Date : %s | Positif : %3d | Classe : %3d | Population : %8d\n",t[i].dep, t[i].date, t[i].pos, t[i].classe, t[i].pop);
     }
 }
 
